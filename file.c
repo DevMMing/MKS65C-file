c671eaf128e08f0d0796291cda325e874a382544
Matthew Ming <34111403+DevMMing@users.noreply.github.com>
2018-10-29 00:59:02 -0400

    fixed file.c

--- file.c
+++ file.c
@@ -4,13 +4,11 @@
 int main(void) {
   char *c = (char *) calloc(100, sizeof(char)); 
   int file = open("hasan.ahnaf.txt",O_RDWR);
-  int something= write(file, "apples and oranges\n",strlen("apples and oranges\n"));
+  write(file, "apples and oranges\n",strlen("apples and oranges\n"));
   close(file);
   int file2 = open("hasan.ahnaf.txt",O_RDWR);
-  printf("%d\n",file2);
-  int duh=read(file2, c, 100);
-  printf("c = % s\n", c);
-
+  read(file2, c, 100);
+  printf("This is what is in the file...\n % s\n", c);
  close(file);
   return 0;
 }
