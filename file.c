589fd452c1e3f394a405fac2427a0f9968ca9223
Matthew Ming <34111403+DevMMing@users.noreply.github.com>
2018-10-29 00:54:20 -0400

    fixed file.c

--- file.c
+++ file.c
@@ -3,10 +3,10 @@
 #include<fcntl.h> 
 int main(void) {
   char *c = (char *) calloc(100, sizeof(char)); 
-  int file = open("dumb.txt",O_RDWR);
+  int file = open("hasan.ahnaf.txt",O_RDWR);
   int something= write(file, "apples and oranges\n",strlen("apples and oranges\n"));
   close(file);
-  int file2 = open("dumb.txt",O_RDWR);
+  int file2 = open("hasan.ahnaf.txt",O_RDWR);
   printf("%d\n",file2);
   int duh=read(file2, c, 100);
   printf("c = % s\n", c);
