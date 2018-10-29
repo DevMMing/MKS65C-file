df33f98eff77c1ea53a506d16aae1658814e5ec1
Matthew Ming <34111403+DevMMing@users.noreply.github.com>
2018-10-29 01:04:37 -0400

    fixed file.c

--- file.c
+++ file.c
@@ -4,11 +4,16 @@
 int main(void) {
   char *c = (char *) calloc(100, sizeof(char)); 
   int file = open("hasan.ahnaf.txt",O_RDWR);
-  write(file, "apples and oranges\n",strlen("apples and oranges\n"));
+  int something= write(file, "apples and oranges\n",strlen("apples and oranges\n"));
+  printf("what integer represents file?/ What is the file descriptor?\n%d\n", file);
+  printf("How many bytes were written?\n%d\n",something);
   close(file);
   int file2 = open("hasan.ahnaf.txt",O_RDWR);
-  read(file2, c, 100);
-  printf("This is what is in the file...\n % s\n", c);
+  printf("what integer represents file2?/ What is the file descriptor?\n%d\n", file2);
+  int duh=read(file2, c, 100);
+  printf("How many bytes were read?\n%d\n",duh);
+  printf("What is hidden within file??!\n%s\n", c);
+
  close(file);
   return 0;
 }
