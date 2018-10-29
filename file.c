f78eab114a3e3821ac8dcb16fbe6cdcf6bacbe75
Matthew Ming <34111403+DevMMing@users.noreply.github.com>
2018-10-29 00:52:12 -0400

    added file.c

--- file.c
+++ file.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<fcntl.h> 
+int main(void) {
+  char *c = (char *) calloc(100, sizeof(char)); 
+  int file = open("dumb.txt",O_RDWR);
+  int something= write(file, "apples and oranges\n",strlen("apples and oranges\n"));
+  close(file);
+  int file2 = open("dumb.txt",O_RDWR);
+  printf("%d\n",file2);
+  int duh=read(file2, c, 100);
+  printf("c = % s\n", c);
 
+ close(file);
+  return 0;
+}
